f311c9de047a549389a14d9765c7540df72d91df
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2023-04-28 09:58:16 +0200

    feat: add defscene-fulcro

--- src/portfolio/fulcro.clj
+++ src/portfolio/fulcro.clj
@@ -11,3 +11,17 @@
   (when (portfolio/portfolio-active?)
     `(portfolio.data/register-collection!
       ~@(portfolio/get-collection-options opts))))
+
+(defmacro defscene-fulcro [id & opts]
+  (when (portfolio/portfolio-active?)
+    (let [Root-sym (gensym "Root")
+          option-map (portfolio/get-options-map id (:line &env) (butlast opts))]
+      `(let [~'component-fn (:component-fn ~option-map)
+             ~'Root-component
+             (com.fulcrologic.fulcro.components/defsc ~Root-sym
+               [~'this ~'props ~'computed-props]
+               ~(:fulcro-opts option-map)
+               ~(last opts))
+             ~'option-map (assoc ~option-map :component-fn (fn [& ~'args] ~Root-sym))]
+         (portfolio.data/register-scene!
+          (portfolio.fulcro/create-scene ~'option-map))))))
